941d3f2a94da166e5c96ed5e0ac19c909611499d
Cassiano Castro <development.castro@gmail.com>
2024-03-05 17:34:13 -0300

    Refactor: moved static keyword to begin of class attribute declaration

--- src/model/dao/ConnectionSingleton.java
+++ src/model/dao/ConnectionSingleton.java
@@ -9,7 +9,7 @@ import java.sql.*;
 public class ConnectionSingleton
 {
 
-    private static Connection connection;
+    static private Connection connection;
 
     private ConnectionSingleton() {}
 
@@ -23,6 +23,7 @@ public class ConnectionSingleton
                 config.getPassword()
             );
         }
+
         return connection;
     }
 }
