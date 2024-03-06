ec312857e1270e6ab321a1a6a74140443ebb50ee
Cassiano Castro <development.castro@gmail.com>
2024-03-06 17:51:21 -0300

    Changed order declaration of the class methods

--- src/model/dao/PersonDAO.java
+++ src/model/dao/PersonDAO.java
@@ -17,31 +17,6 @@ public class PersonDAO
         this.connection = connection;
     }
 
-    public List read() throws SQLException
-    {
-        List list  = new ArrayList();
-        String SQL = "SELECT * FROM pessoas ORDER BY nome";
-
-        try (
-			PreparedStatement ps = this.connection.prepareStatement(SQL);
-            ResultSet rs = ps.executeQuery()
-		)
-        {
-            while ( rs.next() )
-            {
-                int id              = rs.getInt("ID_Pessoa");
-                String name         = rs.getString("nome");
-                String nacionality  = rs.getString("nacionalidade");
-                char sex            = rs.getString("sexo").charAt(0);
-                java.util.Date date = rs.getDate("dataNasc");
-
-                list.add(new Contact(id, name, sex, date, nacionality));
-            }
-        }
-
-        return list;
-    }
-
     public void create(Contact pessoa) throws SQLException
     {
         String SQL = "INSERT INTO pessoas(nome, sexo, dataNasc, nacionalidade) "
@@ -74,6 +49,43 @@ public class PersonDAO
         }
     }
 
+    public void delete(int id) throws SQLException
+    {
+        String SQL = "DELETE FROM pessoas WHERE ID_Pessoa = ?";
+
+        try ( PreparedStatement ps = this.connection.prepareStatement(SQL) )
+        {
+            ps.setInt(1, id);
+
+            ps.executeUpdate();
+        }
+    }
+    
+    public List read() throws SQLException
+    {
+        List list  = new ArrayList();
+        String SQL = "SELECT * FROM pessoas ORDER BY nome";
+
+        try (
+			PreparedStatement ps = this.connection.prepareStatement(SQL);
+            ResultSet rs = ps.executeQuery()
+		)
+        {
+            while ( rs.next() )
+            {
+                int id              = rs.getInt("ID_Pessoa");
+                String name         = rs.getString("nome");
+                String nacionality  = rs.getString("nacionalidade");
+                char sex            = rs.getString("sexo").charAt(0);
+                java.util.Date date = rs.getDate("dataNasc");
+
+                list.add(new Contact(id, name, sex, date, nacionality));
+            }
+        }
+
+        return list;
+    }
+    
     public Contact findByID(int codigo) throws SQLException
     {
         String SQL = "SELECT * FROM pessoas WHERE ID_Pessoa = ?";
@@ -96,18 +108,6 @@ public class PersonDAO
             }
 
             return null;
-        }
-    }
-
-    public void delete(int id) throws SQLException
-    {
-        String SQL = "DELETE FROM pessoas WHERE ID_Pessoa = ?";
-
-        try ( PreparedStatement ps = this.connection.prepareStatement(SQL) )
-        {
-            ps.setInt(1, id);
-
-            ps.executeUpdate();
         }
     }
 }
